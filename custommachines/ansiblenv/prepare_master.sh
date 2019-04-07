493883494ad7f82c0461b676306d0396991d6ca1
Szabó Dániel Ernő <r3ap3rpy@BUD-L-3ZVVQQ2.itron.com>
2019-04-07 12:37:37 +0200

    Adjusted ansible env

--- custommachines/ansiblenv/prepare_master.sh
+++ custommachines/ansiblenv/prepare_master.sh
@@ -11,6 +11,8 @@ echo "AMaster" >> /etc/ansible/hosts
 echo "centosA" >> /etc/ansible/hosts
 echo "centosB" >> /etc/ansible/hosts
 echo "raspberry" >> /etc/ansible/hosts
+echo "reaperdome" >> /etc/ansible/hosts
+echo "reaper" >> /etc/ansible/hosts
 echo "" >> /etc/ansible/hosts
 echo "[agents]" >> /etc/ansible/hosts
 echo "centosA" >> /etc/ansible/hosts
@@ -21,11 +23,22 @@ echo "" >> /etc/ansible/hosts
 echo "[centos]" >> /etc/ansible/hosts
 echo "centosA" >> /etc/ansible/hosts
 echo "centosB" >> /etc/ansible/hosts	
+echo "rhel" >> /etc/ansible/hosts	
+echo "AMaster" >> /etc/ansible/hosts	
 echo "" >> /etc/ansible/hosts
 echo "[master]" >> /etc/ansible/hosts
 echo "AMaster" >> /etc/ansible/hosts
+echo "" >> /etc/ansible/hosts
+echo "[dc]" >> /etc/ansible/hosts
+echo "reaperdome" >> /etc/ansible/hosts
+echo "" >> /etc/ansible/hosts
+echo "[win]" >> /etc/ansible/hosts
+echo "reaper" >> /etc/ansible/hosts
 
 echo "192.168.0.190 AMaster" >> /etc/hosts
 echo "192.168.0.191 centosA" >> /etc/hosts
 echo "192.168.0.192 centosB" >> /etc/hosts
 echo "192.168.0.150 raspberry" >> /etc/hosts
+echo "192.168.0.200 reaperdome" >> /etc/hosts
+echo "192.168.0.201 reaper" >> /etc/hosts
+echo "192.168.0.210 rhel" >> /etc/hosts
